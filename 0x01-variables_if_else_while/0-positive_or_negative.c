ef2fcfd5c7403637678aa2ee1dd08d716503e367
Fotiemb <fotiemb@gmail.com>
2022-03-11 10:47:01 -0800

    task_zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,23 @@
-#include <stdio.h>                                                                                               
-#include <stdlib.h>                                                                                              
-#include <time.h>                                                                                                
-/**                                                                                                              
- *main - entry point                                                                                             
- *Description:prints the last digit of a number                                                                  
- *Return: Always zero (0)                                                                                        
- */                                                                                                              
-int main(void)                                                                                                   
-{                                                                                                                
-int n, last;                                                                                                     
-srand(time(0));                                                                                                  
-n = rand() - RAND_MAX / 2;                                                                                       
-last = n % 10;                                                                                                   
-if (n > 5)                                                                                                       
-printf("Last digit of %d is %d and is greater than 5\n", n, last);                                               
-else if (n == 0)                                                                                                 
-printf("Last digit of %d is %d and is 0\n", n, last);                                                            
-else if (n < 6 && n != 0)                                                                                        
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);                                        
-return (0);                                                                                                      
-}   
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+return (0);
+}
+  
