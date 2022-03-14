67edf6c4d5659fee1ae2d4c4f4857967cde625e9
Fotiemb <fotiemb@gmail.com>
2022-03-14 10:56:04 -0700

    6-print_numberZ.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- this is the entry point
+ * Return: Always return 0 (Success0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9 ; n++)
+	{
+		putchar('0' + n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
