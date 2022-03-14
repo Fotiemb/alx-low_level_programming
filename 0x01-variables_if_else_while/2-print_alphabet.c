0ce70234dac56ac2145b6a394afe82eedf687d34
Fotiemb <fotiemb@gmail.com>
2022-03-14 07:18:36 -0700

    task_var

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-prints the alphabet in lowercase
+ * putchar example: printing the alphabet*/
+int main(void)
+{
+	char c;
+
+for (c = 'a' ; c <= 'a' ; c++)
+{
+	putchar (c);
+}
+
+return (0);
+}
