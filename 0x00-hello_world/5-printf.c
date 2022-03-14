1959223f084d5848439274c350f2fb0e1581f711
Fotiemb <fotiemb@gmail.com>
2022-03-14 04:14:45 -0700

    putsAndPrintf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-printf("with proper grammar, but the outcome is a piece of art,\n"); return 0;
+#include <stdio.h>
+/**
+Use the function printf
+You are not allowed to use the function puts
+Your program should return 0
+Your program should compile without warning when using the -Wall gcc option
+**/
+
+main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
