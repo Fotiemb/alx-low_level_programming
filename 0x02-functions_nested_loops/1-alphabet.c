24b21a61743985a911f3746dc26c4608a5d35f74
Fotiemb <fotiemb@gmail.com>
2022-03-16 11:35:02 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include "main.h"
 
-/*main - print alphabet function
+/**
+ * main - check the code
+ *
  * Return: Always return 0 (Success)
- */
-
+*/
 void print_alphabet(void)
 {
-        char ch;
-        for(ch = 'a' ; ch <= 'z' ; ch++)
-        {
-                 putchar(ch);
-        }
+	int letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		fputchar(letter);
+		letter++;
+	}
 }
