2cf4f3467e01c7722ea81b151facfd40afd6fd13
Fotiemb <fotiemb@gmail.com>
2022-03-18 01:23:55 -0700

    debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/*
+ * main - point of entry
+ *
+ *positive_or_negative - return a value
+
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	positive_or_negative();
+
+	return (0);
+}
