2a080d213bc1201dd9904b9aaf342b7ecc244f2b
Fotiemb <fotiemb@gmail.com>
2022-03-21 16:40:10 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _puts - print a chaine
+ * @str: chaine
+ */
+
+
+void _puts(char *str)
+{
+	puts( &*str);
+}
