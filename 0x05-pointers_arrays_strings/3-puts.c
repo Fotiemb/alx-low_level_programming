c5816cad66cf47d2f1a646700baea5369f1efc4c
Fotiemb <fotiemb@gmail.com>
2022-03-26 05:40:32 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * _puts - print a chaine
- * @str: chaine
- */
-
+* _puts - give a string followed by a new line
+* @str: paramettre
+*/
 
 void _puts(char *str)
 {
-	puts( &*str);
+	puts(&*str);
 }
