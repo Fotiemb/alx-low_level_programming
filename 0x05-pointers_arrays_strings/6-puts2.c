d4d1fc4cfae4817f52a742208ee8971834e28bcf
root <you@example.com>
2022-06-13 16:12:34 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - print every other character
+ * @str: the char to  check
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (i < strlen(str))
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
