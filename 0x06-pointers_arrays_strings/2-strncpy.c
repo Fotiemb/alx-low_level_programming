0f1a5fff19a37b6041df6ba9dec5e70f2837a0ef
root <you@example.com>
2022-06-16 11:06:03 -0700

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strncpy - for copying purpose
+ * @dest: first param
+ * @src: second param
+ * @n: 3rd param
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n)
+
+	return (dest);
+}
+
