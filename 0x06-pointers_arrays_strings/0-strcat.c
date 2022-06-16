88e3560b277ad84d10d420d0de2fa6f2f0d8a615
root <you@example.com>
2022-06-16 10:23:23 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,17 @@
 
 char *_strcat(char *dest, char *src);
 {
-	char *tmp = dest;
+	int len = 0, i;
 
-	for ( ; *dest; dest++);
-	for ( ; *dest = *src; dest++, src++);
-	*dest = '\0';
-
-	return (tmp);
+	while (dest[len])
+	{
+		len++;
+	}
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
+	return (dest);
 }
