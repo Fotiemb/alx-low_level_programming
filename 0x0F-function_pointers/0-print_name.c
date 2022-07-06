3fb43d231a53ea58024da533be66649053f879ed
root <you@example.com>
2022-07-06 12:29:47 -0700

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+able file 15 lines (14 sloc) 242 Bytes
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+  *print_name - prints a name.
+  *@name: pointer to name.
+  *@f: function pointer.
+  *
+  *Return: void.
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
